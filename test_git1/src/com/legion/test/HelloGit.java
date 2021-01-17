12db4905d4bcfb70bbc701858812018e42e3b428
legion <legion@LAPTOP-LEGION>
2021-01-17 08:39:49 +0800

    Create project test_git1 for test.

---
+++ test_git1/src/com/legion/test/HelloGit.java
@@ -0,0 +1,10 @@
+package com.legion.test;
+
+import java.util.Date;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("HelloGit.main(), now date is "+new Date());
+	}
+}
