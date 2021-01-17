15db68e0a1363f14816cb66df78a47a261900855
legion <legion@LAPTOP-LEGION>
2021-01-17 08:56:49 +0800

    update HelloGit, override toString method.

--- test_git1/src/com/legion/test/HelloGit.java
+++ test_git1/src/com/legion/test/HelloGit.java
@@ -7,4 +7,11 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit.main(), now date is "+new Date());
 	}
+	
+	@Override
+	public String toString() {
+		System.out.println("HelloGit.toString(), now date is "+new Date());
+		return "HelloGit.toString()";
+		
+	}
 }
