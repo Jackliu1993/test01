1db7f55b96868a464b551d0d33aa2beea4dd72b6
legion <legion1993@163.com>
2021-01-18 17:02:27 +0800

    Add equal method for TestCodetes

--- 01-git-test1/src/com/bjsxt/service/TestCodetes.java
+++ 01-git-test1/src/com/bjsxt/service/TestCodetes.java
@@ -10,4 +10,10 @@ public class TestCodetes {
 	public static void testMethod() {
 		System.out.println("TestCodetes.testMethod()");
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("TestCodetes.equals()");
+		return super.equals(obj);
+	}
 }
